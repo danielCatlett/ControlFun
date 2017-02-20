ef144d9f12bf8e6ff80a292f8f2b030c31eed3d4
danielCatlett <daniel.catlett42@gmail.com>
2017-02-20 03:00:44 -0600

    Fun with controls
    Got an error starting with the switches, too tired to figure it out.
    Going to bed now.

--- ControlFun/ViewController.swift
+++ ControlFun/ViewController.swift
@@ -1,25 +1,55 @@
-//
 //  ViewController.swift
 //  ControlFun
-//
 //  Created by Daniel Catlett on 2/19/17.
 //  Copyright © 2017 Daniel Catlett. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var leftSwitch: UISwitch!
+    @IBOutlet weak var rightSwitch: UISwitch!
+   // @IBOutlet weak var toggle: UISegmentedControl!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        sliderLabel.text = "50"
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func textFieldDoneEditing(sender:UITextField)
+    {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func onTapGestureRecognized(_ sender: Any)
+    {
+        nameField.resignFirstResponder()
+        numberField.resignFirstResponder()
+    }
+    
+    @IBAction func onSliderChanged(_ sender: UISlider)
+    {
+        sliderLabel.text = "\(lroundf(sender.value))"
+    }
+    
+    @IBAction func onSwitchChanged(_ sender: UISwitch)
+    {
+        let setting = sender.isOn
+        leftSwitch.setOn(setting, animated: true)
+        rightSwitch.setOn(setting, animated: true)
+    }
+    
+    @IBAction func toggleControls(_ sender: UISegmentedControl)
+    {
+    
+    }
 }
 
